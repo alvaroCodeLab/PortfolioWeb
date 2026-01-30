4e1c286069982c4e90000cab5a9ee02b0970c7b3
alvaroCodeLab <alvaroserracarra@gmail.com>
2026-01-30 21:28:12 +0100

    Se añade un fondo interactivo de particulas para el portfolio

--- app/page.tsx
+++ app/page.tsx
@@ -1,5 +1,13 @@
+import CoverParticles from "@/components/cover-particles"
+
 export default function Home() {
   return (
-    <p>From Home</p>
+    <main>
+      <div className="flex min-h-screen h-full bg-no-repeat bg-gradient-cover">
+        <CoverParticles />
+        <p>Introducción</p>
+      </div>
+
+    </main>
   );
 }
