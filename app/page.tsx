546a1afc1c145a0ba3f675ef9a9b5ca01c108a6c
alvaroCodeLab <alvaroserracarra@gmail.com>
2026-02-01 20:54:35 +0100

    Se añadesn nuevos elementos a la introducción del home de la página

--- app/page.tsx
+++ app/page.tsx
@@ -1,23 +1,14 @@
 "use client";
 
-import { motion } from "framer-motion";
 import CoverParticles from "@/components/cover-particles";
+import Introduction from "@/components/introduction";
 
 export default function Home() {
   return (
     <main>
-      <div className="flex min-h-screen h-full bg-[linear-gradient(90.21deg,rgba(10,10,40,0.8)_-5.91%,rgba(0,30,80,0.7)_50%,rgba(0,60,120,0.6)_111.58%)] relative overflow-hidden">
+      <div className="relative min-h-screen overflow-hidden bg-[linear-gradient(90.21deg,rgba(10,10,40,0.8)_-5.91%,rgba(0,30,80,0.7)_50%,rgba(0,60,120,0.6)_111.58%)]">
         <CoverParticles />
-
-        {/* Animación con framer motion */}
-        <motion.div
-          className="absolute top-1/2 left-1/2 transform -translate-x-1/2 -translate-y-1/2 text-white text-4xl font-bold"
-          initial={{ opacity: 0, y: -50 }}
-          animate={{ opacity: 1, y: 0 }}
-          transition={{ duration: 1, ease: "easeOut" }}
-        >
-          Introducción
-        </motion.div>
+        <Introduction />
       </div>
     </main>
   );
