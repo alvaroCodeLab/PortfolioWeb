65a7a7574c51bdd060aae5beac658d6e408c8d71
alvaroCodeLab <alvaroserracarra@gmail.com>
2026-02-13 20:23:56 +0100

    Solución de problema en el header

--- app/(routes)/about/page.tsx
+++ app/(routes)/about/page.tsx
@@ -1,9 +1,13 @@
+import ContainerPage from "@/components/container";
 import TransitionPage from "@/components/TransitionPage";
 
 const PageAboutMe = () => {
     return (
         <>
             <TransitionPage />
+            <ContainerPage>
+                <p>From Children</p>
+            </ContainerPage>
         </>
     )
 }
