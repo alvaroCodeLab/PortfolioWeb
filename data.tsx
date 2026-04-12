a95e334c0c7a0764f8b2e0b8fb861c6b22eba24f
alvaroCodeLab <alvaroserracarra@gmail.com>
2026-04-12 20:17:28 +0200

    Se eliminan de data.tsx los datos que no se estan utilizando y se añade mailto.

--- data.tsx
+++ data.tsx
@@ -1,22 +1,4 @@
-import {
-  BookText,
-  CodeSquare,
-  HomeIcon,
-  UserRound,
-  Linkedin,
-  Twitter,
-  Rss,
-  Twitch,
-  Youtube,
-  Crop,
-  Pencil,
-  Computer,
-  Book,
-  Rocket,
-  Speech,
-  Github,
-  Instagram,
-} from "lucide-react";
+import { Linkedin, Github, Instagram, Mail } from "lucide-react";
 
 export const socialNetworks = [
   {
@@ -34,38 +16,10 @@ export const socialNetworks = [
     logo: <Instagram size={30} strokeWidth={1} />,
     src: "https://www.instagram.com/alvarosc_03",
   },
-];
-
-export const itemsNavbar = [
-  {
-    id: 1,
-    title: "Home",
-    icon: <HomeIcon size={25} color="#fff" strokeWidth={1} />,
-    link: "/",
-  },
-  {
-    id: 2,
-    title: "User",
-    icon: <UserRound size={25} color="#fff" strokeWidth={1} />,
-    link: "/about-me",
-  },
-  {
-    id: 3,
-    title: "Book",
-    icon: <BookText size={25} color="#fff" strokeWidth={1} />,
-    link: "/services",
-  },
   {
     id: 4,
-    title: "Target",
-    icon: <CodeSquare size={25} color="#fff" strokeWidth={1} />,
-    link: "/portfolio",
-  },
-  {
-    id: 5,
-    title: "Home",
-    icon: <Speech size={25} color="#fff" strokeWidth={1} />,
-    link: "/testimonials",
+    logo: <Mail size={30} strokeWidth={1} />,
+    src: "mailto:ejemplo@ejemplo.com",
   },
 ];
 
@@ -93,70 +47,6 @@ export const dataAboutPage = [
     description:
       "Gestión y actualización de bases de datos de alumnos y cursos, inserción de datos en Control COM y soporte en procesos administrativos internos.",
     date: "Abr 2023",
-  },
-];
-
-export const dataCounter = [
-  {
-    id: 0,
-    endCounter: 10,
-    text: "Años de experiencia",
-    lineRight: true,
-    lineRightMobile: true,
-  },
-  {
-    id: 1,
-    endCounter: 80,
-    text: "Clientes satisfechos",
-    lineRight: true,
-    lineRightMobile: false,
-  },
-  {
-    id: 2,
-    endCounter: 220,
-    text: "Proyectos finalizados",
-    lineRight: true,
-    lineRightMobile: true,
-  },
-  {
-    id: 3,
-    endCounter: 30,
-    text: "Premios ganadores",
-    lineRight: false,
-    lineRightMobile: false,
-  },
-];
-
-export const serviceData = [
-  {
-    icon: <Crop />,
-    title: "Branding",
-    description:
-      "Desarrollo de una identidad de marca sólida y coherente, incluyendo diseño de logotipo, colores y elementos visuales",
-  },
-  {
-    icon: <Pencil />,
-    title: "Diseño web",
-    description:
-      "Diseño creativo y profesional de interfaces web intuitivas y atractivas, centradas en la experiencia del usuario",
-  },
-  {
-    icon: <Computer />,
-    title: "Desarrollo web",
-    description:
-      "Diseño y desarrollo de sitios web a medida, adaptados a tus necesidades",
-  },
-  {
-    icon: <Book />,
-    title: "Copywriting",
-    description:
-      "Creación de contenido persuasivo y atractivo que capta la atención de tu audiencia",
-  },
-  {
-    icon: <Rocket />,
-    title: "SEO",
-    description:
-      "Optimización de tu presencia en línea mediante estrategias de SEO avanzadas, ",
   },
 ];
 
@@ -195,7 +85,7 @@ export const dataPages = [
     rol: "Desarrollador Fullstack",
     company: "Alberto Cristalero",
     image: "/profile1.png",
-    url:"https://albertocristalero.es",
+    url: "https://albertocristalero.es",
   },
   {
     id: 2,
@@ -204,33 +94,17 @@ export const dataPages = [
     rol: "Desarrollador Fullstack",
     company: "Lucy Sal",
     image: "/profile1.png",
-    url:"https://lucysal.com",
+    url: "https://lucysal.com",
   },
 ];
 
 export const skills = [
-  {
-    name: "Creatividad",
-  },
-  {
-    name: "Trabajo en equipo",
-  },
-  {
-    name: "Comunicación",
-  },
-  {
-    name: "Resolución de problemas",
-  },
-  {
-    name: "Pensamiento crítico",
-  },
-  {
-    name: "Adaptabilidad",
-  },
-  {
-    name: "Gestión del tiempo",
-  },
-  {
-    name: "Aprendizaje rápido",
-  },
+  { name: "Creatividad" },
+  { name: "Trabajo en equipo" },
+  { name: "Comunicación" },
+  { name: "Resolución de problemas" },
+  { name: "Pensamiento crítico" },
+  { name: "Adaptabilidad" },
+  { name: "Gestión del tiempo" },
+  { name: "Aprendizaje rápido" },
 ];
